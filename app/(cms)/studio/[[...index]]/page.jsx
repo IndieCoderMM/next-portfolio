3862d1ada8e0bac4435aa5be88f9c3c6d87a4cae
IndieCoderMM <hthant00chk@gmail.com>
2024-11-03 16:27:06 +0630

    Clear js version

--- app/(cms)/studio/[[...index]]/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { NextStudio } from "next-sanity/studio";
-import config from "@/sanity.config";
-
-const Studio = () => <NextStudio config={config} />;
-
-export default Studio;
--- schemas/index.js
+++
@@ -1,5 +0,0 @@
-import profile from "./profile";
-import project from "./project";
-import skill from "./skill";
-
-export const schemaTypes = [profile, skill, project];
