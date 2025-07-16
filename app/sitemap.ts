259fa8a25647c54aa6641cdbdfe0ad2b0f97d611
IndieCoderMM <hthant00chk@gmail.com>
2025-07-16 13:05:49 +0630

    Filter ext links

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,10 +10,12 @@ export default async function sitemap() {
     lastModified: new Date(project._updatedAt).toISOString(),
   }));
 
-  const routes = navLinks.map(({ href: route }) => ({
-    url: `${siteUrl}${route !== "/" ? route : ""}`,
-    lastModified: new Date().toISOString(),
-  }));
+  const routes = navLinks
+    .filter((l) => l.type !== "external")
+    .map(({ href: route }) => ({
+      url: `${siteUrl}${route !== "/" ? route : ""}`,
+      lastModified: new Date().toISOString(),
+    }));
 
   return [...routes, ...projects];
 }
